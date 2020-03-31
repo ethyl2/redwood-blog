ef65801b48b297c2b81c5da94d52be98338be598
Heather Nuffer <zzzuzz@gmail.com>
2020-03-31 12:11:36 -0600

    Adds some styling, especially to HomePage

--- web/src/components/BlogPost/BlogPost.js
+++ web/src/components/BlogPost/BlogPost.js
@@ -1,6 +1,30 @@
 import { Link, routes } from '@redwoodjs/router'
 
 const BlogPost = ({ post }) => {
+  const dtf = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  })
+
+  const formatDate = (dateString) => {
+    // "2020-03-20T00:00:00.000Z" -> "dd Month YYYY"
+    if (dateString.length === 24) {
+      const choppedDateString = dateString.slice(0, 10)
+      const originalDate = new Date(choppedDateString)
+      const adjustedDate = new Date(originalDate)
+      adjustedDate.setDate(adjustedDate.getDate() + 1)
+      const [
+        { value: mo },
+        ,
+        { value: da },
+        ,
+        { value: ye },
+      ] = dtf.formatToParts(adjustedDate)
+      return `${da} ${mo} ${ye}`
+    }
+    return dateString
+  }
   return (
     <article key={post.id}>
       <header>
@@ -9,7 +33,7 @@ const BlogPost = ({ post }) => {
         </h2>
       </header>
       <h3>
-        <i>Posted at: {post.createdAt}</i>
+        <i>Posted on {formatDate(post.createdAt)}</i>
       </h3>
       <p>{post.body}</p>
       <hr />
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,20 @@
 import BlogLayout from 'src/layouts/BlogLayout'
 import BlogPostsCell from 'src/components/BlogPostsCell'
 
+import trees from 'src/images/trees.jpg'
+
 const HomePage = () => {
   return (
     <BlogLayout>
       <h1>Blog Posts</h1>
-      <BlogPostsCell />
+      <div className="home-page-main-content">
+        <div className="blog-posts-cell-container">
+          <BlogPostsCell />
+        </div>
+        <div className="img-container">
+          <img src={trees} alt="forest" />
+        </div>
+      </div>
     </BlogLayout>
   )
 }
